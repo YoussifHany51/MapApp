db898709a2ac103325cf3d24779b843a1f54c413
Youssif Hany <youssifhany962@gmail.com>
2022-02-17 18:32:57 +0200

    Initial Commit

---
+++ MapsApp/MapsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsAppApp.swift
+//  MapsApp
+//
+//  Created by Youssif Hany on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
