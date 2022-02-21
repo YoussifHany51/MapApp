6d9a6a08a6e144041c03d24012a73583b98cfe02
Youssif Hany <youssifhany962@gmail.com>
2022-02-21 19:46:51 +0200

    Optimize

---
+++ MapsApp/Views/AnnotationView.swift
@@ -0,0 +1,20 @@
+//
+//  AnnotationView.swift
+//  MapsApp
+//
+//  Created by Youssif Hany on 19/02/2022.
+//
+
+import SwiftUI
+
+struct AnnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnnotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnnotationView()
+    }
+}
---
+++ MapsApp/Views/LocationPerviewView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationPerviewView.swift
+//  MapsApp
+//
+//  Created by Youssif Hany on 18/02/2022.
+//
+
+import SwiftUI
+
+struct LocationPerviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationPerviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationPerviewView()
+    }
+}
